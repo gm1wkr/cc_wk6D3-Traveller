d94bbeee54c12b7e04c25ae58f4bc611978a8411
GM1WKR <gm1wkr@gmail.com>
2022-06-22 17:52:06 +0100

    initial commit.

---
+++ models/traveller.js
@@ -0,0 +1,68 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  let startLocations = [];
+  this.journeys.map(journey => {
+    startLocations.push(journey.startLocation);
+  });
+  return startLocations;
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  let endLocations = [];
+  this.journeys.map(journey => {
+    endLocations.push(journey.endLocation);
+  });
+  return endLocations;
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  let journeysByTransport = [];
+  this.journeys.filter( journey => {
+    if(journey.transport === transport){
+      journeysByTransport.push(journey);
+    }
+  });
+  return journeysByTransport;
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  journeyList = [];
+  this.journeys.filter(journey => {
+    if(journey.distance > minDistance){
+      journeyList.push(journey);
+    }
+  });
+  return journeyList;
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce( (totalDistance, journey ) =>{
+    return totalDistance + journey.distance;
+  }, 0 );
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const unique = (value, index, self) => {
+    return self.indexOf(value) === index
+  }
+
+  const transportTypesList = [];
+  this.journeys.map(journey => {
+    transportTypesList.push(journey.transport);
+  });
+
+  const uniqueTransports = transportTypesList.filter(unique);
+  return uniqueTransports;
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function(){
+  return this.journeys.reduce( (totalDistance, journey ) =>{
+    return totalDistance + journey.distance;
+  }, 0 );
+};
+
+
+module.exports = Traveller;
